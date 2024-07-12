d9b9507a9c00270efbc7228fdf23fa5905f01684
MiningXL <miningxl@gmail.com>
2024-07-12 20:17:12 +0200

    updated readme; now debug switch works

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # SGJ2024
 SGJ2024
+## Installation
+
+Create python environment:
+```shell
+python3.10 -m venv venv
+. venv/bin/activate
+```
+
+Install the project:
+```shell
+pip install -e .
+```
+
+## Usage
+Launch the game:
+```shell
+launch_game
+```
+(Optional) Launch in debug mode if you do not have a leapmotion device available:
+```shell
+launch_game --debug
+```
--- sgj2024/run.py
+++ sgj2024/run.py
@@ -1,8 +1,16 @@
 import arcade
+import argparse
 
 from sgj2024.gamewindow import GameWindow
 from sgj2024.config import *
 
 def main():
-    game = GameWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--no-leapmotion', '-L', action='store_true', help='Do not use leap motion')
+    args = parser.parse_args()
+    
+    if args.debug:
+        print("Launching Game in Debug Mode")
+    game = GameWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, debug=args.debug)
     arcade.run()

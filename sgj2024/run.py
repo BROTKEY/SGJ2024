57266ad4f440264d12963649a8cee13b19f2295f
MiningXL <miningxl@gmail.com>
2024-07-12 21:18:31 +0200

    I definetly didn't copy paste this

--- sgj2024/run.py
+++ sgj2024/run.py
@@ -7,7 +7,6 @@ from sgj2024.config import *
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--debug', action='store_true')
-    parser.add_argument('--no-leapmotion', '-L', action='store_true', help='Do not use leap motion')
     args = parser.parse_args()
     
     if args.debug:

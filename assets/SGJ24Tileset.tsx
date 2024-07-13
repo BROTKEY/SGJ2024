6c3e21c51352112a492384c1d19bf01af069c0f0
BROTKEY <54113864+BROTKEY@users.noreply.github.com>
2024-07-13 15:35:45 +0200

    level edits

--- assets/SGJ24Tileset.tsx
+++ assets/SGJ24Tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="SGJ24Tileset" tilewidth="512" tileheight="1024" tilecount="11" columns="0">
+<tileset version="1.8" tiledversion="1.8.0" name="SGJ24Tileset" tilewidth="512" tileheight="1024" tilecount="12" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="256" height="256" source="SGJ24TILES/asset1.png"/>
@@ -33,5 +33,8 @@
  </tile>
  <tile id="13">
   <image width="256" height="512" source="SGJ24TILES/cactus_2_psd.png"/>
+ </tile>
+ <tile id="15">
+  <image width="256" height="512" source="SGJ24TILES/flying_cactus.png"/>
  </tile>
 </tileset>

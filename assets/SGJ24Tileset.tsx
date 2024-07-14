93d610e61710f9d14703fd35f086a3df35b029bf
MiningXL <miningxl@gmail.com>
2024-07-14 13:56:52 +0200

    added the new tiles

--- assets/SGJ24Tileset.tsx
+++ assets/SGJ24Tileset.tsx
@@ -1,20 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="SGJ24Tileset" tilewidth="512" tileheight="1024" tilecount="24" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="SGJ24Tileset" tilewidth="512" tileheight="1024" tilecount="33" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image source="SGJ24TILES/asset1.png" width="256" height="256"/>
+  <image width="256" height="256" source="SGJ24TILES/asset1.png"/>
  </tile>
  <tile id="2">
-  <image source="SGJ24TILES/Background.png" width="256" height="1024"/>
+  <image width="256" height="1024" source="SGJ24TILES/Background.png"/>
  </tile>
  <tile id="3">
-  <image source="SGJ24TILES/bridge.png" width="256" height="256"/>
+  <image width="256" height="256" source="SGJ24TILES/bridge.png"/>
  </tile>
  <tile id="4">
-  <image source="SGJ24TILES/bridge_2.png" width="256" height="256"/>
+  <image width="256" height="256" source="SGJ24TILES/bridge_2.png"/>
  </tile>
  <tile id="6">
-  <image source="SGJ24TILES/Cloud_1.png" width="512" height="512"/>
+  <image width="512" height="512" source="SGJ24TILES/Cloud_1.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="76" y="197.333" width="364" height="112">
     <ellipse/>
@@ -22,22 +22,22 @@
   </objectgroup>
  </tile>
  <tile id="7">
-  <image source="SGJ24TILES/dirt_2.png" width="256" height="256"/>
+  <image width="256" height="256" source="SGJ24TILES/dirt_2.png"/>
  </tile>
  <tile id="8">
-  <image source="SGJ24TILES/Le_Ball.png" width="256" height="256"/>
+  <image width="256" height="256" source="SGJ24TILES/Le_Ball.png"/>
  </tile>
  <tile id="9">
-  <image source="SGJ24TILES/Umbrella.png" width="256" height="256"/>
+  <image width="256" height="256" source="SGJ24TILES/Umbrella.png"/>
  </tile>
  <tile id="11">
-  <image source="SGJ24TILES/water_2.png" width="256" height="256"/>
+  <image width="256" height="256" source="SGJ24TILES/water_2.png"/>
  </tile>
  <tile id="12">
-  <image source="SGJ24TILES/dirt_3.png" width="256" height="256"/>
+  <image width="256" height="256" source="SGJ24TILES/dirt_3.png"/>
  </tile>
  <tile id="13">
-  <image source="SGJ24TILES/cactus_2_psd.png" width="256" height="512"/>
+  <image width="256" height="512" source="SGJ24TILES/cactus_2_psd.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="121.998" y="87.2115">
     <polygon points="0,0 -48.5053,10.7789 -91.1311,79.8622 -97.5005,200.39 -75.9426,367.464 -59.7742,424.789 23.0278,425.278 31.3569,340.517 65.1636,332.678 109.259,298.871 125.428,254.775 119.058,224.398 124.502,51.2885 91.0019,19.7885"/>
@@ -45,20 +45,20 @@
   </objectgroup>
  </tile>
  <tile id="16">
-  <image source="SGJ24TILES/not_yet_sploding_cola.png" width="256" height="1024"/>
+  <image width="256" height="1024" source="SGJ24TILES/not_yet_sploding_cola.png"/>
  </tile>
  <tile id="17">
-  <image source="SGJ24TILES/sploding_cola_1.png" width="256" height="1024"/>
+  <image width="256" height="1024" source="SGJ24TILES/sploding_cola_1.png"/>
   <animation>
    <frame tileid="17" duration="300"/>
    <frame tileid="18" duration="300"/>
   </animation>
  </tile>
  <tile id="18">
-  <image source="SGJ24TILES/sploding_cola_2.png" width="256" height="1024"/>
+  <image width="256" height="1024" source="SGJ24TILES/sploding_cola_2.png"/>
  </tile>
  <tile id="19">
-  <image source="SGJ24TILES/flying_cactus.png" width="256" height="512"/>
+  <image width="256" height="512" source="SGJ24TILES/flying_cactus.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="71.6322" y="67.539">
     <polygon points="0,0 -18.4197,31.3817 -19.1019,60.7169 -20.4664,296.762 -39.5683,311.771 -39.5683,322.686 -32.064,330.873 -20.4664,382.721 56.6236,412.738 130.985,386.814 146.676,329.508 155.544,320.64 155.544,309.724 136.442,300.173 157.591,243.55 154.18,227.177 135.078,202.617 139.853,74.3611 143.947,42.2971 125.527,3.41106 82.5476,-17.0553 41.6149,-15.0087"/>
@@ -66,30 +66,57 @@
   </objectgroup>
  </tile>
  <tile id="20">
-  <image source="SGJ24TILES/eee53f1bae834b35.png" width="512" height="512"/>
+  <image width="512" height="512" source="SGJ24TILES/eee53f1bae834b35.png"/>
  </tile>
  <tile id="21">
-  <image source="SGJ24TILES/Cursor.png" width="256" height="256"/>
+  <image width="256" height="256" source="SGJ24TILES/Cursor.png"/>
  </tile>
  <tile id="22">
-  <image source="SGJ24TILES/palm.png" width="512" height="768"/>
+  <image width="512" height="768" source="SGJ24TILES/palm.png"/>
  </tile>
  <tile id="23">
-  <image source="SGJ24TILES/Cursor_texture.png" width="256" height="256"/>
+  <image width="256" height="256" source="SGJ24TILES/Cursor_texture.png"/>
  </tile>
  <tile id="50">
-  <image source="SGJ24TILES/winter_tree.png" width="256" height="512"/>
+  <image width="256" height="512" source="SGJ24TILES/winter_tree.png"/>
  </tile>
  <tile id="51">
-  <image source="SGJ24TILES/ice_1.1.png" width="256" height="256"/>
+  <image width="256" height="256" source="SGJ24TILES/ice_1.1.png"/>
  </tile>
  <tile id="52">
-  <image source="SGJ24TILES/ice_top_.png" width="256" height="256"/>
+  <image width="256" height="256" source="SGJ24TILES/ice_top_.png"/>
  </tile>
  <tile id="53">
-  <image source="SGJ24TILES/waterfall_bottom_2.png" width="256" height="256"/>
+  <image width="256" height="256" source="SGJ24TILES/waterfall_bottom_2.png"/>
  </tile>
  <tile id="54">
-  <image source="SGJ24TILES/waterfall_midng.png" width="256" height="256"/>
+  <image width="256" height="256" source="SGJ24TILES/waterfall_midng.png"/>
+ </tile>
+ <tile id="55">
+  <image width="512" height="512" source="SGJ24TILES/Corner_house.png"/>
+ </tile>
+ <tile id="56">
+  <image width="169" height="181" source="SGJ24TILES/Snowman.png"/>
+ </tile>
+ <tile id="57">
+  <image width="512" height="512" source="SGJ24TILES/snowbackground_deocration_.png"/>
+ </tile>
+ <tile id="58">
+  <image width="256" height="256" source="SGJ24TILES/snow_background_decoration.png"/>
+ </tile>
+ <tile id="59">
+  <image width="512" height="512" source="SGJ24TILES/big_ice_slide.png"/>
+ </tile>
+ <tile id="60">
+  <image width="256" height="256" source="SGJ24TILES/waterfall_top.png"/>
+ </tile>
+ <tile id="61">
+  <image width="256" height="256" source="SGJ24TILES/ice_platform.png"/>
+ </tile>
+ <tile id="62">
+  <image width="256" height="256" source="SGJ24TILES/snow_wood_top.png"/>
+ </tile>
+ <tile id="63">
+  <image width="256" height="256" source="SGJ24TILES/winter_platform_mid.png"/>
  </tile>
 </tileset>

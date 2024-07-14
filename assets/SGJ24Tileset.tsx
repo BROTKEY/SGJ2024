284c4ac904c822fa8f8cf521181932fc0a9042b4
MiningXL <miningxl@gmail.com>
2024-07-14 14:06:11 +0200

    added seperate spawn tile for clarity

--- assets/SGJ24Tileset.tsx
+++ assets/SGJ24Tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="SGJ24Tileset" tilewidth="512" tileheight="1024" tilecount="33" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="SGJ24Tileset" tilewidth="512" tileheight="1024" tilecount="34" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="256" height="256" source="SGJ24TILES/asset1.png"/>
@@ -118,5 +118,8 @@
  </tile>
  <tile id="63">
   <image width="256" height="256" source="SGJ24TILES/winter_platform_mid.png"/>
+ </tile>
+ <tile id="64">
+  <image width="256" height="256" source="SGJ24TILES/Spawn.png"/>
  </tile>
 </tileset>
